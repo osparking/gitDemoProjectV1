05f969ba4af46feaad1ed37d0bc66028093e1856
Park, JongBum <jbpark03@gmail.com>
2020-02-13 22:45:07 +0900

    Commit all files first time

---
+++ src/gitDemoProjectV1/ProjectMain.java
@@ -0,0 +1,12 @@
+package gitDemoProjectV1;
+
+import java.util.logging.Logger;
+
+public class ProjectMain {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요?");
+		Logger.getGlobal().info("반갑습니다!");
+	}
+
+}
