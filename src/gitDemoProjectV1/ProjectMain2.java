b426e7deff93efcc155ca80711d6b9f442f469ab
Park, JongBum <jbpark03@gmail.com>
2020-02-14 15:40:29 +0900

    add one more line

--- src/gitDemoProjectV1/ProjectMain2.java
+++ src/gitDemoProjectV1/ProjectMain2.java
@@ -6,6 +6,7 @@ public class ProjectMain2 {
 
 	public static void main(String[] args) {
 		System.out.println("안녕하세요?");
+		System.out.println("또 만났군요.");
 		Logger.getGlobal().info("반갑습니다!");
 	}
 
